40c3e6b782784db3289f524d15daed1e765c24c4
Ahmadali <ahmadali.azizov@outlook.com>
2018-05-07 02:42:43 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,4 +2,6 @@ def speak_to_grandma(loud)
   if loud == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
     elsif loud == loud.upcase
+    return "NO, NOT SINCE 1938!"
+  else
     return 
